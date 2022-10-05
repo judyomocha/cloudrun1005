38d634457d3e7f4387c1dc992eaa1c97f7c08538
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-05 16:07:40 +0900

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,12 @@
+  import os
+  import discord
+  import python-dotenv
+  import load_dotenv
+  import requests
+  import json
+pip install --upgrade pydata-google-auth
+pip install google-api-python-client
+pip install --upgrade pip
+pip install google
+pip install gspread
+pip install oauth2client
