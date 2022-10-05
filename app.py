4695f5cb9818bc8376bd0a88d439f6947c833715
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-05 16:06:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,43 +15,48 @@
 import signal
 import sys
 from types import FrameType
-
 from flask import Flask
-
 from utils.logging import logger
-
 app = Flask(__name__)
+from google.oauth2.service_account import Credentials
+import gspread
+import discord
+import json
+import os
+from dotenv import load_dotenv
+# .envファイルの内容を読み込見込む
+load_dotenv()
+TOKEN = os.environ['TOKEN']
+GUILD_ID = os.environ['GUILD_ID']
+CHANNEL_ID = os.environ['CHANNEL_ID']
+SPREADSHEET_KEY = os.environ['SPREADSHEET_KEY']
+GCP_SA_KEY = os.environ['GCP_SA_KEY']
+SHEET_NAME = os.environ['SHEET_NAME']
+
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+parsed = json.loads(GCP_SA_KEY)
+gc = gspread.service_account_from_dict(parsed)
+sh = gc.open_by_key(SPREADSHEET_KEY)
+ws = sh.worksheet(SHEET_NAME)
+list_of_lists = ws.get_all_values()
+i = len(list_of_lists[0])
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
 
 
-@app.route("/")
-def hello() -> str:
-    # Use basic logging with custom fields
-    logger.info(logField="custom-entry", arbitraryField="custom-entry")
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-    # https://cloud.google.com/run/docs/logging#correlate-logs
-    logger.info("Child logger with trace Id.")
+    if message.channel.id != int(CHANNEL_ID):
+        return
 
-    return "Hello, World!"
+    if message.channel.id == int(CHANNEL_ID):
+        ws.update_cell(i+1,1,message.content )
+        print(f'更新します @{message.author}!') 
+        await message.channel.send(f'更新します {message.author}!') 
 
-
-def shutdown_handler(signal_int: int, frame: FrameType) -> None:
-    logger.info(f"Caught Signal {signal.strsignal(signal_int)}")
-
-    from utils.logging import flush
-
-    flush()
-
-    # Safely exit program
-    sys.exit(0)
-
-
-if __name__ == "__main__":
-    # Running application locally, outside of a Google Cloud Environment
-
-    # handles Ctrl-C termination
-    signal.signal(signal.SIGINT, shutdown_handler)
-
-    app.run(host="localhost", port=8080, debug=True)
-else:
-    # handles Cloud Run container termination
-    signal.signal(signal.SIGTERM, shutdown_handler)
+client.run(TOKEN)
