bb9565b1d8a6db644ab429e13081781fe823458b
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-06 12:24:42 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,11 +4,7 @@
   import load_dotenv
   import requests
   import json
-
-sudo add-apt-repository ppa:mc3man/trusty-media
-sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get -y install ffmpeg
+pip install google-cloud-storage
 pip install google-cloud-speech
 pip install --upgrade pydata-google-auth
 pip install google-api-python-client
@@ -17,3 +13,13 @@ pip install google
 pip install gspread
 pip install oauth2client
 pip3.10 install python-dotenv
+pip install --upgrade google-cloud-storage
+sudo apt update
+sudo apt install python3 python3-dev python3-venv
+sudo apt-get install wget
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+sudo add-apt-repository ppa:mc3man/trusty-media
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get -y install ffmpeg
