bf759030756023ca18453279eff81894c2ac5e81
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-06 09:13:09 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,8 +4,6 @@
   import load_dotenv
   import requests
   import json
-  import spreadsheet
-pip install gspread-dataframe
 pip install --upgrade pydata-google-auth
 pip install google-api-python-client
 pip install --upgrade pip
