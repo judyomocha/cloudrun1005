f1f3c1c902819ae1e3d14c9b35f78c7a3a59239f
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-12 07:14:45 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,11 +4,24 @@
   import load_dotenv
   import requests
   import json
-  import spreadsheet
-pip install gspread-dataframe
+
 pip install --upgrade pydata-google-auth
 pip install google-api-python-client
 pip install --upgrade pip
 pip install google
-pip install gspread
 pip install oauth2client
+pip install google-api-python-client google-auth-httplib2 google-auth-oauthlib
+pip install google-cloud-storage
+pip install google-cloud-texttospeech
+pip install --upgrade google-cloud-texttospeech
+pip3 install python-dotenv
+pip install --upgrade google-cloud-storage
+pip install structlog
+pip install python-dotenv
+pip install ffmpeg
+pip install pip install pynacl
+pip install PyNaCl
+pip3 install PyNaCl library
+pip install numpy
+pip install pydub
+sudo apt-get -y install ffmpeg
