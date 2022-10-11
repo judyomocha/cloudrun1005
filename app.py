2537c81f43e72a732285a878e8f003eddea86933
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-12 05:27:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,36 +27,41 @@ from dotenv import load_dotenv
 # .envファイルの内容を読み込見込む
 load_dotenv()
 TOKEN = os.environ['TOKEN']
-GUILD_ID = os.environ['GUILD_ID']
-CHANNEL_ID = os.environ['CHANNEL_ID']
-SPREADSHEET_KEY = os.environ['SPREADSHEET_KEY']
 GCP_SA_KEY = os.environ['GCP_SA_KEY']
-SHEET_NAME = os.environ['SHEET_NAME']
 
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-parsed = json.loads(GCP_SA_KEY)
-gc = gspread.service_account_from_dict(parsed)
-sh = gc.open_by_key(SPREADSHEET_KEY)
-ws = sh.worksheet(SHEET_NAME)
-list_of_lists = ws.get_all_values()
-i = len(list_of_lists[0])+1
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents: Intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 
 @client.event
+async def on_ready():
+    print('Login!!!')
+
+
+@client.event
+@client.event
 async def on_message(message):
-    if message.author == client.user:
+    global voiceChannel
+    if message.author.bot:
+        return
+    if message.author.voice.channel:
+        if message.content == '!con':
+            voiceChannelｌ = await VoiceChannel.connect(message.author.voice.channel)
+            await message.channel.send('読み上げるよ！')
+            return
+        if message.content == '!en':
+            VoiceChannel.stop()
+            await message.channel.send('またね！')
+            await VoiceChannel.disconnect()
+            return
+        elif message.content != '!con' or '!en':
+            tts = gTTS(message.content)
+            tts.save("hello.mp3")
+            message.guild.voice_client.play(discord.FFmpegPCMAudio("hello.mp3"))
+            return
+    else:
         return
 
-    if message.channel.id != int(CHANNEL_ID):
-        return
-
-    if message.channel.id == int(CHANNEL_ID):
-        ws.update_cell(i,1,message.content )
-        print(f'更新します @{message.author}!') 
-        await message.channel.send(f'更新します {message.author}!') 
 
 client.run(TOKEN)
