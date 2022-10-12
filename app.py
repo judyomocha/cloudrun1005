d5eb1aecdfdbf62f9941f6f7061c089d132c9b06
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-12 11:49:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,45 +18,73 @@ from types import FrameType
 from flask import Flask
 from utils.logging import logger
 app = Flask(__name__)
-from google.oauth2.service_account import Credentials
-import gspread
 import discord
 import json
 import os
+from gtts import gTTS
 from dotenv import load_dotenv
+from collections import defaultdict, deque
+from pathlib import Path
+from discord import Intents
+from apiclient.discovery import build
+from discord.player import FFmpegPCMAudio
+from discord.channel import VoiceChannel
 # .envファイルの内容を読み込見込む
 load_dotenv()
 TOKEN = os.environ['TOKEN']
-GUILD_ID = os.environ['GUILD_ID']
-CHANNEL_ID = os.environ['CHANNEL_ID']
-SPREADSHEET_KEY = os.environ['SPREADSHEET_KEY']
-GCP_SA_KEY = os.environ['GCP_SA_KEY']
-SHEET_NAME = os.environ['SHEET_NAME']
 
-scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-parsed = json.loads(GCP_SA_KEY)
-gc = gspread.service_account_from_dict(parsed)
-sh = gc.open_by_key(SPREADSHEET_KEY)
-ws = sh.worksheet(SHEET_NAME)
-list_of_lists = ws.get_all_values()
-i = len(list_of_lists[0])+1
+from google.oauth2.service_account import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.cloud import texttospeech
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
 
-intents = discord.Intents.default()
-intents.message_content = True
+
+intents: Intents = discord.Intents.all()
 client = discord.Client(intents=intents)
-
+voiceChannel: VoiceChannel 
 
 @client.event
+async def on_ready():
+    print('Login!!!')
+
+@client.event
+@client.event
 async def on_message(message):
-    if message.author == client.user:
+    global voiceChannel
+    if message.author.bot:
         return
-
-    if message.channel.id != int(CHANNEL_ID):
-        return
-
-    if message.channel.id == int(CHANNEL_ID):
-        ws.update_cell(i,1,message.content )
-        print(f'更新します @{message.author}!') 
-        await message.channel.send(f'更新します {message.author}!') 
+    if message.author.voice.channel:
+         text = message.content
+         if message.content == '!con':
+            voiceChannel = await VoiceChannel.connect(message.author.voice.channel)
+            await message.channel.send('読み上げるよ！')
+            return
+         if message.content == '!en':
+            VoiceChannel.stop()
+            await message.channel.send('またね！')
+            await VoiceChannel.disconnect()
+            return
+         elif message.content != '!con' or '!en':
+              from google.cloud import texttospeech
+              client = texttospeech.TextToSpeechClient()
+              synthesis_input = texttospeech.SynthesisInput(text=text)
+              voice = texttospeech.VoiceSelectionParams(
+                  language_code="ja-JP", 
+                  ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+              )
+              audio_config = texttospeech.AudioConfig(
+                   audio_encoding=texttospeech.AudioEncoding.MP3
+              )
+              response = client.synthesize_speech(
+                  input=synthesis_input, voice=voice, audio_config=audio_config
+              )
+              with open("hello.mp3", "wb") as out:
+                  out.write(response.audio_content)
+                  print('Audio content written to file "hello.mp3"')
+                  message.guild.voice_client.play(discord.FFmpegPCMAudio("hello.mp3"))
+         return
 
 client.run(TOKEN)
+            
