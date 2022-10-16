85c39f448d702cf0690f361d90093cfc2e301042
judyomocha <112790395+judyomocha@users.noreply.github.com>
2022-10-16 14:04:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,8 @@ parsed = json.loads(GCP_SA_KEY)
 gc = gspread.service_account_from_dict(parsed)
 sh = gc.open_by_key(SPREADSHEET_KEY)
 ws = sh.worksheet(SHEET_NAME)
-
+list_of_lists = ws.get_all_values()
+i = len(list_of_lists[0])+1
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -54,10 +55,12 @@ async def on_message(message):
         return
 
     if message.channel.id == int(CHANNEL_ID):
-        list_of_lists = ws.get_all_values()
-        i = len(list_of_lists[0])+1
-        ws.update_cell(i,1,f'更新します @{message.author}!' )
-        await message.channel.send(f'更新します @{message.author}!')
-        print(message.id) 
+        ws.update_cell(i+1,1,message.content )
+        print(f'更新します @{message.author}!') 
+        await message.channel.send(f'更新します {message.author}!') 
+        
+        
+        
+        
 
 client.run(TOKEN)
